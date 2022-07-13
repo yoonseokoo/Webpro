0960c604a420002305167b46cb6e1ca57e72c02c
yoonseokoo <yoonseokoo@gmail.com>
2022-07-13 18:12:10 +0900

    202207132

---
+++ Source/08.Spring/ch05_aop/src/main/java/com/lec/ch05/ex1/Student.java
@@ -0,0 +1,18 @@
+package com.lec.ch05.ex1;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	private String name;
+	private int age;
+	private int gradeNum;
+	private int classNum;
+	
+	public void getStudentInfo() { //CORE function to be called (target method = business logic)
+		System.out.println("Name : " + name);
+		System.out.println("Age : " + age);
+		System.out.println("Grade  : " + gradeNum);
+		System.out.println("Class Number : " + classNum);
+	}
+}
---
+++ Source/08.Spring/ch05_aop/src/main/java/com/lec/ch05/ex1/TestMain.java
@@ -0,0 +1,14 @@
+package com.lec.ch05.ex1;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TestMain {
+	public static void main(String[] args) {
+		String location = "classpath:META-INF/applicationCTX1.xml";
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(location);
+		Student student = ctx.getBean("student", Student.class);
+		Worker worker = ctx.getBean("worker", Worker.class);
+		student.getStudentInfo();
+		worker.getWorkerInfo();
+	}
+}
